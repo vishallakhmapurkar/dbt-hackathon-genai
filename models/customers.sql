6187377574776d2b2f89cfc4c9c10d15a37a9d04
vishal <lakhmapurkar.vishal@gmail.com>
2025-08-31 10:33:00 +0000

    added orders and stg

--- models/customers.sql
+++ models/customers.sql
@@ -1,28 +1,12 @@
-{{
-  config(
-    materialized='view'
-  )
-}}
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.vishal_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.vishal_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
